e1d6a100c76eadd33f3f2c2fe330c02cbaa6a02b
ShihanZhang44419 <s3524808@student.rmit.edu.au>
2019-10-13 06:49:46 +0000

    debug

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,7 +3,7 @@ if Rails.env.production?
   #   config.fog_credentials = {
   #     # Configuration for Amazon S3
   #     :provider              => 'AWS',
-  #     :region                => 'ap-southeast-2',
+  #     
   #     :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
   #     :aws_secret_access_key => ENV['S3_SECRET_KEY']
   #   }
@@ -13,11 +13,12 @@ if Rails.env.production?
 
  CarrierWave.configure do |config|
     config.storage    =  :aws                  # required
-    config.aws_region =  'ap-southeast-2'
+    #config.aws_region =  'ap-southeast-2'
     config.aws_bucket =  ENV['S3_BUCKET']     # required
     config.aws_acl    =  :public_read
 
     config.aws_credentials = {
+      :region                => 'ap-southeast-2',
       access_key_id:      ENV['S3_ACCESS_KEY'],     # required
       secret_access_key:  ENV['S3_SECRET_KEY']    # required
     }
