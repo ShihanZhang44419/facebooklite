4e84b37e54bad1f82bef6aaa83d9718b60417e25
ShihanZhang44419 <s3524808@student.rmit.edu.au>
2019-10-13 06:18:33 +0000

    debug

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,6 +3,7 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
+      :region                => 'ap-southeast-2',
       :aws_access_key_id     => 'AKIAROIXOOTMEU3Q72F3',
       :aws_secret_access_key => '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'
     }
