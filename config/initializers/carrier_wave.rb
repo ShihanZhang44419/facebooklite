6c11836e58223fb8e586740aa6de732a584d2eef
ShihanZhang44419 <s3524808@student.rmit.edu.au>
2019-10-19 09:58:55 +0000

    debug

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -11,17 +11,4 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
   end
 
-# CarrierWave.configure do |config|
-#     config.storage    =  :aws                  # required
-#     #config.aws_region =  'ap-southeast-2'
-#     config.aws_bucket =  ENV['S3_BUCKET']     # required
-#     config.aws_acl    =  :public_read
-
-#     config.aws_credentials = {
-#       :region                => 'ap-southeast-2',
-#       access_key_id:      ENV['S3_ACCESS_KEY'],     # required
-#       secret_access_key:  ENV['S3_SECRET_KEY']    # required
-#     }
-    
-#   end
 end
